315880c56512c1ef3c5124b88e5409449bee9f7f
Rafael Dias Silveira <rafa197795@gmail.com>
2018-06-08 16:40:42 -0300

    Updated Yarn lockfile

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReacDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 
 const root = document.getElementById('app');
 
-const jsx = (
-  <div>
-    <h1>QS React</h1>
-  </div>
-);
+// const jsx = (
+//   <div>
+//     <h1>QS React</h1>
+//   </div>
+// );
 
-ReacDOM.render(jsx, root);
+ReactDOM.render(<h1>QS React</h1>, root);
 
