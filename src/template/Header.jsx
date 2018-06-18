126a2f48413ab4e713b63a6326c045313dd85fd8
Rafael Dias <rafa197795@gmail.com>
2018-06-17 21:09:20 -0300

    Definido a estrutura básica do header, onde o usuário terá acesso a barra de pesquisa, ao dashboard de usuário e ao dashboard de livros

--- src/template/Header.jsx
+++ src/template/Header.jsx
@@ -17,6 +17,7 @@ const Header = () => (
       <span className="navbar-toggler-icon" />
     </button>
 
+
     <div className="collapse navbar-collapse" id="navbarText">
 
       <ul className="navbar-nav mr-auto">
@@ -30,7 +31,14 @@ const Header = () => (
           <NavLink className="nav-link" to="/books" exact>Livros</NavLink>
         </li>
       </ul>
+
     </div>
+
+    <form className="form-inline">
+      <input type="search" className="form-control mr-sm-2" placeholder="Search" aria-label="Search" />
+      <button className="btn btn-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+
   </nav>
 );
 
