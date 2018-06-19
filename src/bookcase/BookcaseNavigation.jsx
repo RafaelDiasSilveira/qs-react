f809e689552a6fde6d25419a24a7ab6b8d03f9e1
Rafael Dias <rafa197795@gmail.com>
2018-06-19 09:31:33 -0300

    Minha estante iniciado - #18

--- src/bookcase/BookcaseNavigation.jsx
+++ src/bookcase/BookcaseNavigation.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import { NavLink } from 'react-router-dom';
+
 export default () => (
   <div>
     <h3 className="text-center">Total de livros na estante: 0/10</h3>
-    <button className="btn btn-primary btn-lg btn-block">
+
+    <NavLink to="/book/new" className="btn btn-primary btn-lg btn-block">
       Adicionar livro
-    </button>
+    </NavLink>
   </div>
 );
