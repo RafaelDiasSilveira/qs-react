e138d54403aece3e311ebe5f374d30f374902e6b
Rafael Dias <rafa197795@gmail.com>
2018-06-20 19:52:40 -0300

    Finalizaçao das mudanças de state do BookForm

--- src/book/BookForm.jsx
+++ src/book/BookForm.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 
 import 'react-dates/initialize';
-import { SingleDatePicker } from 'react-dates';
+// import { SingleDatePicker } from 'react-dates';
 
 class BookForm extends React.Component {
   constructor(props) {
@@ -14,9 +14,7 @@ class BookForm extends React.Component {
       // codigo: props.book ? props.book.codigo : '',
       titulo: props.book ? props.book.titulo : '',
       autor: props.book ? props.book.autor : '',
-      dataPublicacao: props.book ? moment(props.book.dataPublicacao) : moment(),
-      // genero: props.book ? props.book.genero : '',
-      focused: false,
+      dataPublicacao: props.book ? props.book.dataPublicacao : '',
     };
   }
 
@@ -30,10 +28,14 @@ class BookForm extends React.Component {
     this.setState(() => ({ autor }));
   };
 
-  onDateChange = (dataPublicacao) => {
-    if (dataPublicacao) {
-      this.setState(() => ({ dataPublicacao }));
-    }
+  onDateChange = (e) => {
+    const dataPublicacao = e.target.value;
+    this.setState(() => ({ dataPublicacao }));
+  }
+
+  onGeneroChange = (e) => {
+    const genero = e.target.value;
+    this.setState(() => ({ genero }));
   }
 
   onFocusChange = ({ focused }) => {
@@ -49,36 +51,61 @@ class BookForm extends React.Component {
       <div>
         <form onSubmit={this.onSubmit}>
 
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Titulo"
-              value={this.state.titulo}
-              onChange={this.onTituloChange}
-              className="form-control"
-            />
+          <div className="form-row">
+            <div className="col form-group">
+              <label>Título</label>
+              <input
+                type="text"
+                placeholder="Titulo"
+                value={this.state.titulo}
+                onChange={this.onTituloChange}
+                className="form-control"
+              />
+            </div>
+
+            <div className="col form-group">
+              <label>Autor</label>
+              <input
+                type="text"
+                placeholder="Autor"
+                value={this.state.autor}
+                onChange={this.onAutorChange}
+                className="form-control"
+              />
+            </div>
           </div>
 
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Autor"
-              value={this.state.autor}
-              onChange={this.onAutorChange}
-              className="form-control"
-            />
+          <div className="form-row">
+            <div className="col form-group">
+              <label htmlFor="generoSelect">
+              Gênero
+              </label>
+              <select
+                id="generoSelect"
+                className="form-control"
+              >
+                <option disabled defaultValue> -- selecione uma opção -- </option>
+                <option value="epopeia">EPOPEIA</option>
+                <option value="novela">NOVELA</option>
+                <option value="conto">CONTO</option>
+                <option value="ensaio">ENSAIO</option>
+                <option value="romance">ROMANCE</option>
+              </select>
+            </div>
+
+            <div className="col form-group">
+              <label>Data de Publicação</label>
+              <input
+                type="date"
+                className="form-control"
+                value={this.state.dataPublicacao}
+                onChange={this.onDateChange}
+                defaultValue="31/12/18"
+              />
+            </div>
           </div>
 
-          <div>
-            <SingleDatePicker
-              date={this.state.dataPublicacao}
-              onDateChange={this.onDateChange}
-              focused={this.state.focused}
-              onFocusChange={this.onFocusChange}
-              numberOfMonths={1}
-              isOutsideRange={() => false}
-            />
-          </div>
+          <button className="btn btn-success btn-lg btn-block">Adicionar Livro</button>
 
         </form>
       </div>
--- src/tests/book/BookForm.test.js
+++ src/tests/book/BookForm.test.js
@@ -23,25 +23,42 @@ describe('should render BookForm correctly', () => {
   });
 });
 
+describe('should set state correctly on input change', () => {
+  test('when setting titulo input', () => {
+    const value = 'Teste do GG';
+    const wrapper = shallow(<BookForm />);
 
-test('should set titulo state on input change', () => {
-  const value = 'Teste do GG';
-  const wrapper = shallow(<BookForm />);
+    wrapper.find('input').at(0).simulate('change', {
+      target: { value }
+    });
 
-  wrapper.find('input').at(0).simulate('change', {
-    target: { value }
+    expect(wrapper.state('titulo')).toBe(value);
   });
 
-  expect(wrapper.state('titulo')).toBe(value);
-});
+  test('when setting autor input', () => {
+    const value = 'Teste do GG';
+    const wrapper = shallow(<BookForm />);
 
-test('should set autor state on input change', () => {
-  const value = 'Teste do GG';
-  const wrapper = shallow(<BookForm />);
+    wrapper.find('input').at(1).simulate('change', {
+      target: { value }
+    });
 
-  wrapper.find('input').at(1).simulate('change', {
-    target: { value }
+    expect(wrapper.state('autor')).toBe(value);
   });
 
-  expect(wrapper.state('autor')).toBe(value);
-});
+  test('when setting dataPublicacao input', () => {
+    const wrapper = shallow(<BookForm />);
+    const now = moment("2018-12-25");
+
+    wrapper.find('input').at(2).simulate('change', {
+      target: { value: now }
+    });
+
+    console.log(wrapper.state('dataPublicacao'));
+    console.log(now);
+
+    expect(wrapper.state('dataPublicacao')).toBe(now);
+  });
+})
+
+
